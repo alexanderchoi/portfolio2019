69c15a085718b1a7b492ac6b367904832c66ea27
alexanderchoi <alexchoiweb@gmail.com>
2019-10-19 17:19:32 -0700

    small update to html and css

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import { directive, directiveLiteral } from "@babel/types";
 
 class Navbar extends Component {
   render() {
-    return <p>Navbar</p>;
+    return (
+      <div>
+        <h1>Alex Choi</h1>
+        <div id="menu">
+          <ul>
+            <li>Work</li>
+            <li>About</li>
+            <li>Info</li>
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
